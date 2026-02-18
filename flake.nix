5eced678eefeec9f616cb83b08426bf5b4d98641
Louis-Mael Derval <louis-mael.derval@epita.fr>
2026-02-18 23:00:16 +0000

    chore(flake): setup flake base

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A flake for extended C macro procesor";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    systems.url = "github:nix-systems/x86_64-linux";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , ...
+    }: (
+      flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = { };
+
+        devShells = { };
+
+        formatter = pkgs.nixpkgs-fmt;
+      })
+    );
+}
