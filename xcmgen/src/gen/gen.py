bc5303149155dc5c32cb457c9b998302b8c59de3
Louis-Mael Derval <louis-mael.derval@epita.fr>
2026-03-21 11:07:37 +0000

    feat(xcmgen: gen): read configuration from file

--- xcmgen/src/gen/gen.py
+++ xcmgen/src/gen/gen.py
@@ -1,37 +1,37 @@
+from argparse import ArgumentParser
 from jinja2 import Environment, PackageLoader, select_autoescape
+import toml
 
 from utils import name_utils
 
 
-def generate():
+def create_argument_parser() -> ArgumentParser:
+    parser = ArgumentParser(
+        prog="xcmgen",
+        description="AST Node generator for xcmp",
+    )
+    parser.add_argument("-c", "--config", type=str)
+    return parser
+
+
+def create_environment() -> Environment:
     env = Environment(
         loader=PackageLoader("gen"),
         autoescape=select_autoescape(),
     )
     env.filters["kebab"] = name_utils.kebab
+    return env
+
+
+def read_config(config_path: str) -> dict:
+    config = toml.load((config_path))
+    return config
+
+
+def generate():
+    parser = create_argument_parser()
+    args = parser.parse_args()
+    config = read_config(args.config)
+    env = create_environment()
     node_hh_template = env.get_template("node.hh.jinja")
-    node = {
-        "name": "Text",
-        "sys_includes": [
-            "<string>",
-        ],
-        "includes": [],
-        "local_includes": [
-            "ast.hh",
-        ],
-        "members": [
-            {
-                "name": "text",
-                "store_type": "std::string",
-                "r_type": "const std::string&",
-                "w_type": "std::string&",
-                "W_type": "const std::string&",
-                "ctor": True,
-                "r_acc": True,
-                "w_acc": False,
-                "W_acc": False,
-                "default_value": None,
-            },
-        ],
-    }
-    print(node_hh_template.render(node=node))
+    print(node_hh_template.render(node=config["nodes"][0]))
